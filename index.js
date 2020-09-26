875d427803c66209c78cd02f388773a78169ad63
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-26 23:39:08 +0500

    3.11 phonebook full stack

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", (req, res) => {
   return Object.keys(req.body).length ? JSON.stringify(req.body) : "";
