8f4eb1d71a7a47be0bfee4c9cd8ff48be9f92ce2
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-27 21:10:37 +0500

    3.21 Deploying the database backend to production

---
+++ build/precache-manifest.c0ac28a449ffab4ac944f11c5fa08afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923d47c265ef906c49c675458e51fe35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cbf2e4733bb5249566c",
+    "url": "/static/css/main.b657f708.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbf2e4733bb5249566c",
+    "url": "/static/js/main.8a521d66.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
