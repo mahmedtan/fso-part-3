2235f96736e7a726254b4801c2f2578c8dfcf471
Muhammad Ahmed <mahmedexec@gmail.com>
2020-09-27 21:18:56 +0500

    3.21 Deploying the database backend to production

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app
     Person.findByIdAndUpdate(params.id, body, {
       new: true,
       runValidators: true,
-      context: query,
+      context: "query",
     })
       .then((person) => {
         if (person) res.json(person);
